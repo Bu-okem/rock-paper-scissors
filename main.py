7d2a1319088a854ebe7df7d9cf1305d28fd9c0dd
Buokem <41740816+Bu-okem@users.noreply.github.com>
2022-06-09 08:52:43 +0100

    Create loop
    Using loop to allow players to play again without rerunning the game

--- main.py
+++ main.py
@@ -6,37 +6,45 @@
 #     pass
 
 import random
-while True:
+repeat = True
+while repeat:
     choices = ['r', 'p', 's']
     cpu = random.choice(choices)
     player = input("Pick an option from r, p, s:\n").lower()
     if player == cpu:
         print(cpu)
         print("It's a tie")
-        break
+        # break
     elif player == 'r' and cpu == 'p':
         print(cpu)
         print('You lose')
-        break
+        # break
     elif player == 'r' and cpu == 's':
         print(cpu)
         print('You win')
-        break
+        # break
     elif player == 'p' and cpu == 'r':
         print(cpu)
         print('You win')
-        break
+        # break
     elif player == 'p' and cpu == 's':
         print(cpu)
         print('You lose')
-        break
+        # break
     elif player == 's' and cpu == 'p':
         print(cpu)
         print('You win')
-        break
+        # break
     elif player == 's' and cpu == 'r':
         print(cpu)
         print('You lose')
-        break
+        # break
     else:
-        pass
+        print("Wrong input, try again")
+
+    play_again = input("Do you want to play again? y/n\n").lower()
+
+    if play_again == "y":
+        repeat = True
+    elif play_again == "n":
+        repeat = False
